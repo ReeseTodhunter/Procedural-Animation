097b9b32fabd33e24f53fc9e83516fa4e0da44f8
ReeseTodhunter <reeset538@gmail.com>
2023-05-27 06:44:40 +0100

    Leg Armature Classes Setup

---
+++ Assets/IK_LegArmature.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IK_LegArmature : InverseKinematicArm
+{
+    #region Armature Target Movement Variables
+
+    [SerializeField]
+    private float stepTime = 1.0f; //Time taken to complete a step
+
+    [SerializeField]
+    private float strideLength = 5.0f; //Total length each step should go
+
+    [SerializeField]
+    private float stepHeight = 5.0f; //Amount to raise end of armature during steps
+
+    protected Vector3 previousTargetPosition; //Position of the last target
+    protected Vector3 midPointPosition; //Stores the middle position of target movement
+    protected Vector3 newTargetPosition; //Position of the new target
+
+    protected Vector3 targetMovement; //Stores amount to move Target by
+
+    protected float horizontalElapsedTime; //Total time passed moving armature target horizontally
+    protected float verticalElapsedTime; //Total time passed moving armature target vertically
+
+    protected float horizontalPercentageComplete; //Percentage horizontal movement update is done
+    protected float verticalPercentageComplete; //Percentage vertical movement update is done
+
+    #endregion
+
+    #region Update Armature
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+        UpdateTargetPosition();
+    }
+
+    #endregion
+
+    #region Calculate Target Motion
+
+    private void UpdateTargetPosition()
+    {
+        //If the current target position is not at the new Target position
+        if (target.position != newTargetPosition)
+        {
+            //Update the total elapsed time moving
+            horizontalElapsedTime += Time.deltaTime;
+            verticalElapsedTime += Time.deltaTime;
+
+            //Calculate how far through the movement the target should be
+            horizontalPercentageComplete = horizontalElapsedTime / stepTime;
+            verticalPercentageComplete = verticalElapsedTime / (stepTime / 2);
+
+            //Update the horizontal movement position
+            targetMovement = Vector3.Lerp(previousTargetPosition, newTargetPosition, horizontalPercentageComplete);
+
+            //If in the first half of the movement raise the target's position
+            if (horizontalPercentageComplete < 0.5)
+            {
+                targetMovement.y = Vector3.Lerp(previousTargetPosition, midPointPosition, verticalPercentageComplete).y;
+            }
+            //If in the second half of the movement lower the targets position
+            else
+            {
+                targetMovement.y = Vector3.Lerp(midPointPosition, newTargetPosition, verticalPercentageComplete - 1).y;
+            }
+
+            //Update the targets position;
+            target.position = targetMovement;
+            //Return without checking for movement if currently moving
+            return;
+        }
+        CheckMovement();
+    }
+
+    private void CheckMovement()
+    {
+        //Cast a ray from the root of the armature to the floor ahead
+        if (Physics.Raycast(bones[0].position, new Vector3(1, -3, 0).normalized, out var hit, completeLength, LayerMask.GetMask("Default"), QueryTriggerInteraction.Ignore))
+        {
+            //If the distance from the ray hit point to the current target position is greater than the set stride length update the new target position
+            if (Vector3.Distance(hit.point, target.position) >= strideLength)
+            {
+                //Get the previous targeted position
+                previousTargetPosition = target.position;
+                //Get the middle point of the previous target and the new target and add the step height to this
+                midPointPosition = (hit.point - previousTargetPosition) / 2 + previousTargetPosition;
+                midPointPosition.y += stepHeight;
+                
+                //===========================================================
+                //Insert obstacle detection here to avoid kicking an obstacle
+                //===========================================================
+
+                //Update the new targeted position to where the ray hit the ground
+                newTargetPosition = hit.point;
+                
+                //Reset the total elapsed movement time
+                horizontalElapsedTime = 0.0f;
+                verticalElapsedTime = 0.0f;
+            }
+        }
+    }
+
+    #endregion
+}
--- Assets/InverseKinematicArm.cs
+++ Assets/InverseKinematicArm.cs
@@ -8,47 +8,22 @@ public class InverseKinematicArm : MonoBehaviour
     #region Armature Base Variables
 
     [SerializeField]
-    private int chainLength = 3; //Length of the armature
+    protected int chainLength = 3; //Length of the armature
 
     [SerializeField]
-    private Transform target; //Position the armature end will try to reach
+    protected Transform target; //Position the armature end will try to reach
     [SerializeField]
-    private Transform pole; //Position of the pole the armature will attempt to bend towards
+    protected Transform pole; //Position of the pole the armature will attempt to bend towards
 
     [SerializeField]
-    private int iterations = 10; //How many times to iterate the armature's calculations
+    protected int iterations = 10; //How many times to iterate the armature's calculations
     [SerializeField]
-    private float delta = 1.0f; //Minimum desired calculated distance from the target
+    protected float delta = 1.0f; //Minimum desired calculated distance from the target
 
     protected float[] bonesLength; //List of each individual bone length
     protected float completeLength; //How long in total the armature is
     protected Transform[] bones; //Reference to each bone's position
     protected Vector3[] positions; //All of the bones actual positions for calculations before being applied
-
-    #endregion
-
-    #region Armature Target Movement Variables
-
-    [SerializeField]
-    private float stepTime = 1.0f; //Time taken to complete a step
-
-    [SerializeField]
-    private float strideLength = 5.0f; //Total length each step should go
-
-    [SerializeField]
-    private float stepHeight = 5.0f; //Amount to raise end of armature during steps
-
-    protected Vector3 previousTargetPosition; //Position of the last target
-    protected Vector3 midPointPosition; //Stores the middle position of target movement
-    protected Vector3 newTargetPosition; //Position of the new target
-
-    protected Vector3 targetMovement; //Stores amount to move Target by
-
-    protected float horizontalElapsedTime; //Total time passed moving armature target horizontally
-    protected float verticalElapsedTime; //Total time passed moving armature target vertically
-
-    protected float horizontalPercentageComplete; //Percentage horizontal movement update is done
-    protected float verticalPercentageComplete; //Percentage vertical movement update is done
 
     #endregion
 
@@ -88,10 +63,9 @@ public class InverseKinematicArm : MonoBehaviour
 
     #region Update Armature
 
-    private void LateUpdate()
+    protected virtual void LateUpdate()
     {
         InverseKinematics();
-        UpdateTargetPosition();
     }
 
     #endregion
@@ -173,75 +147,8 @@ public class InverseKinematicArm : MonoBehaviour
 
     #endregion
 
-    #region Calculate Target Motion
-
-    private void UpdateTargetPosition()
-    {
-        //If the current target position is not at the new Target position
-        if (target.position != newTargetPosition)
-        {
-            //Update the total elapsed time moving
-            horizontalElapsedTime += Time.deltaTime;
-            verticalElapsedTime += Time.deltaTime;
-
-            //Calculate how far through the movement the target should be
-            horizontalPercentageComplete = horizontalElapsedTime / stepTime;
-            verticalPercentageComplete = verticalElapsedTime / (stepTime / 2);
-
-            //Update the horizontal movement position
-            targetMovement = Vector3.Lerp(previousTargetPosition, newTargetPosition, horizontalPercentageComplete);
-
-            //If in the first half of the movement raise the target's position
-            if (horizontalPercentageComplete < 0.5)
-            {
-                targetMovement.y = Vector3.Lerp(previousTargetPosition, midPointPosition, verticalPercentageComplete).y;
-            }
-            //If in the second half of the movement lower the targets position
-            else
-            {
-                targetMovement.y = Vector3.Lerp(midPointPosition, newTargetPosition, verticalPercentageComplete - 1).y;
-            }
-
-            //Update the targets position;
-            target.position = targetMovement;
-            //Return without checking for movement if currently moving
-            return;
-        }
-        CheckMovement();
-    }
-
-    private void CheckMovement()
-    {
-        //Cast a ray from the root of the armature to the floor ahead
-        if (Physics.Raycast(bones[0].position, new Vector3(1, -3, 0).normalized, out var hit, completeLength, LayerMask.GetMask("Default"), QueryTriggerInteraction.Ignore))
-        {
-            //If the distance from the ray hit point to the current target position is greater than the set stride length update the new target position
-            if (Vector3.Distance(hit.point, target.position) >= strideLength)
-            {
-                //Get the previous targeted position
-                previousTargetPosition = target.position;
-                //Get the middle point of the previous target and the new target and add the step height to this
-                midPointPosition = (hit.point - previousTargetPosition) / 2 + previousTargetPosition;
-                midPointPosition.y += stepHeight;
-                
-                //===========================================================
-                //Insert obstacle detection here to avoid kicking an obstacle
-                //===========================================================
-
-                //Update the new targeted position to where the ray hit the ground
-                newTargetPosition = hit.point;
-                
-                //Reset the total elapsed movement time
-                horizontalElapsedTime = 0.0f;
-                verticalElapsedTime = 0.0f;
-            }
-        }
-    }
-
-    #endregion
-
     #region Gizmos
-    private void OnDrawGizmos()
+    protected virtual void OnDrawGizmos()
     {
         var current = this.transform;
         for (int i = 0; i < chainLength && current != null && current.parent != null; i++)
