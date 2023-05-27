f30c215391fa32bf93e918bccd654dd8b2d110ad
ReeseTodhunter <reeset538@gmail.com>
2023-05-27 21:43:47 +0100

    Walker code update

--- Assets/Walker.cs
+++ Assets/Walker.cs
@@ -4,53 +4,18 @@ using UnityEngine;
 
 public class Walker : MonoBehaviour
 {
-    #region Walker variables
-
-    //Store all armatures
     [SerializeField]
-    private List<InverseKinematicArm> armatures = null;
-    //How long a step should be
-    [SerializeField]
-    private float strideLength = 5.0f;
-    //How high a step should go
-    [SerializeField]
-    private float stepHeight = 5.0f;
-    //How long it should take to take a step
-    [SerializeField]
-    private float stepTime = 1.0f;
-    //Minimum tolerance for foot placement
-    [SerializeField]
-    private float minDistanceToTarget = 0.1f;
+    List<IK_LegArmature> legs = null; //A list of all the walkers legs
 
-    private Vector3 previousTarget = new Vector3();
-    private Vector3 midPoint = new Vector3();
-    private Vector3 newTarget = new Vector3();
-    private float elapsedTime = 0;
-    private float percentageComplete;
-    private float verticalPercentageComplete;
+    public float walkSpeed = 3;
+    public bool turn = false;
 
-    private Vector3 horizontalPosition;
-    private Vector3 verticalPosition;
-    
-
-    #endregion
-
-    #region WalkerInitalisation
-
-    private void Awake()
+    void Update()
     {
-        //If there are no armatures setup destroy this script
-        if (armatures == null) Destroy(this);
+        transform.position += transform.right * Time.deltaTime * walkSpeed;
+        if (turn)
+        {
+            transform.eulerAngles += Vector3.up * Time.deltaTime * (walkSpeed / 2);
+        }
     }
-
-    #endregion
-
-    #region Calculate Target Movement
-
-    private void LateUpdate()
-    {
-
-    }
-
-    #endregion
 }
